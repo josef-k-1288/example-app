4537ee777df32018f15769b6ca655a1bead12667
Aleksa <obradovic.aleksa@gmail.com>
2023-08-10 13:57:26 +0200

    like-unlike

--- app/Http/Controllers/PostLikeController.php
+++ app/Http/Controllers/PostLikeController.php
@@ -7,7 +7,30 @@ use Illuminate\Http\Request;
 
 class PostLikeController extends Controller
 {
-    public function store(Post $post) {
-        dd($post);
+    public function __construct()
+    {   
+        $this->middleware(['auth']);
+    }
+
+
+    public function store(Post $post, Request $request) {
+        //dd($post);
+        //dd($post->likedBy($request->user()));
+
+        if($post->likedBy($request->user())) {
+            return response(null, 409);
+        }
+
+        $post->likes()->create([
+            'user_id' => $request->user()->id,
+        ]);
+
+        return back();
+    }
+
+    public function destroy(Post $post, Request $request) {
+        $request->user()->likes()->where('post_id', $post->id)->delete();
+
+        return back();
     }
 }
